e2439764d76a1c08e526aba19ff479344ef2ae76
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2022-10-12 10:52:50 +0100

    Adds clap argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about=None)]
+#[command(group(
+    ArgGroup::new("vers")
+        .required(true)
+        .args(["set_ver", "major", "minor", "patch"]),
+struct Cli {
+    #[arg(short, long)]
+    command: String,
+    id: Optional<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    println!("Hello, world! {:?}", args.pattern.unwrap());
 }
