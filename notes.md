c26e8ee919a68e2ba267862e39e445db79dbf5a6
André Pereira <adbp@live.com.pt>
2022-10-15 23:11:13 +0100

    fix command arguments

---
+++ notes.md
@@ -0,0 +1,9 @@
+## Commands
+
+- broker
+
+- put <id> <msg>
+
+- get <id>
+- sub <id> 
+- unsub <id>
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use clap::Parser;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
-#[command(group(
-    ArgGroup::new("vers")
-        .required(true)
-        .args(["set_ver", "major", "minor", "patch"]),
 struct Cli {
-    #[arg(short, long)]
     command: String,
-    id: Optional<String>,
+
+    #[arg(short,long)]
+    id: Option<String>,
+
+    #[arg(short,long)]
+    message: Option<String>,
 }
 
 fn main() {
     let args = Cli::parse();
-    println!("Hello, world! {:?}", args.pattern.unwrap());
+    println!("Hello, world! {:?}", args.command);
 }
