83284a9fcee129f96766beea9ce4c6b9feae6cad
Beatriz Aguiar <beatrizaguiar.up@gmail.com>
2022-11-30 18:48:55 +0000

    register
    Co-authored-by: André Pereira <Andrepereira2001@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -2,43 +2,53 @@
 
 import express from "express";
 import node from "./src/node.js";
-import cors from 'cors'
+import cors from "cors";
 
 const PORT = process.env.PORT || 3001;
 
+let currentNode = null;
+node.createNode().then((result) => {
+  currentNode = result;
+});
 
-// REQUEST HANDLERS 
+// REQUEST HANDLERS
 
 const registerHandler = async (req, res) => {
+  const { username, password } = req.body;
+
+  const response = await node.register(currentNode, username, password);
+
+  if (response.hasAttribute("error")) return res.status(400).json(response);
+
   res.status(200).json({
     message: "Registe user",
-    port: createPort()
-  })
-}
+    port: createPort(),
+  });
+};
 
 const loginHandler = async (req, res) => {
   res.status(200).json({
     message: "Login user",
-    port: createPort()
-  })
-  node()
-}
+    port: createPort(),
+  });
+  node();
+};
 
-const logoutHandler = async (req, res) => {}
+const logoutHandler = async (req, res) => {};
 
-const followHandler = async (req, res) => {}
+const followHandler = async (req, res) => {};
 
-const unfollowHandler = async (req, res) => {}
+const unfollowHandler = async (req, res) => {};
 
 const feedHandler = async (req, res) => {
   res.status(200).json({
     message: "Getting feed",
-  })
-}
+  });
+};
 
-const userHandler = async (req, res) => {}
+const userHandler = async (req, res) => {};
 
-const profileHandler = async (req, res) => {}
+const profileHandler = async (req, res) => {};
 
 // ROUTES
 
@@ -51,14 +61,14 @@ const setupRoutes = (app) => {
   app.post("/register", registerHandler);
   app.post("/login", loginHandler);
   app.post("/logout", logoutHandler);
-  
+
   app.post("/follow", followHandler);
   app.post("/unfollow", unfollowHandler);
 
   app.get("/feed", feedHandler);
   app.get("/user", userHandler);
   app.get("/profile", profileHandler);
-}
+};
 
 const createPort = () => {
   const app = express();
@@ -69,17 +79,15 @@ const createPort = () => {
   });
 
   setupRoutes(app);
-  
+
   return backend.address().port;
-}
+};
 
 const app = express();
 app.use(cors());
 
-setupRoutes(app)
+setupRoutes(app);
 
 const backend = app.listen(PORT, () => {
   console.log(`Backend listening on ${backend.address().port}`);
 });
-
-
--- backend/src/node.js
+++ backend/src/node.js
@@ -1,96 +1,124 @@
-import { createLibp2p } from 'libp2p';
-import { tcp } from '@libp2p/tcp';
-import { noise } from '@chainsafe/libp2p-noise';
-import { mplex } from '@libp2p/mplex';
-import { kadDHT } from '@libp2p/kad-dht'
-import { bootstrap } from '@libp2p/bootstrap'
-import PeerId from 'peer-id'
-import { str2array, array2str } from './utils.js';
-import fs from 'fs';
+import { createLibp2p } from "libp2p";
+import { tcp } from "@libp2p/tcp";
+import { noise } from "@chainsafe/libp2p-noise";
+import { mplex } from "@libp2p/mplex";
+import { kadDHT } from "@libp2p/kad-dht";
+import { bootstrap } from "@libp2p/bootstrap";
+import PeerId from "peer-id";
+import { str2array, array2str } from "./utils.js";
+import fs from "fs";
+import { createHash } from 'crypto';
 
 const bootstrapers = [
-    '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ',
-    '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
-    '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-    '/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp',
-    '/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa',
-    '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt'
-  ]
+  "/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmZa1sAxajnQjVM8WjWXoMbmPd7NsWhfKsPkErzpm9wGkp",
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+  "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+];
 
-export default async function node() {
-    const peerId = await PeerId.create()
+export async function node() {
+  const peerId = await PeerId.create();
 
-    const peerIdJson = JSON.stringify(peerId.toJSON())
-    fs.writeFile("peer.txt", peerIdJson, (err) => {
-        console.log(err)
-    })
+  const peerIdJson = JSON.stringify(peerId.toJSON());
+  fs.writeFile("peer.txt", peerIdJson, (err) => {
+    console.log(err);
+  });
 
-    node = await createNode(peerId)
+  node = await createNode(peerId);
 
-    // node.addEventListener('peer:discovery', (event) => {
-    //     // No need to dial, autoDial is on
-    //     console.log('Discovered:', event.detail.id.toString())
-    //     login(node, "/key")
-    // })
-    // // console.log(data)
-    // register(node, "/key", "test")
-    return node
+  // node.addEventListener('peer:discovery', (event) => {
+  //     // No need to dial, autoDial is on
+  //     console.log('Discovered:', event.detail.id.toString())
+  //     login(node, "/key")
+  // })
+  // // console.log(data)
+  // register(node, "/key", "test")
+  return node;
 }
 
-const createNode = async (peerId) => {
-    const node = await createLibp2p({
-        // peerId: peerId,
-        addresses: {
-            // add a listen address (localhost) to accept TCP connections on a random port
-            listen: ['/ip4/127.0.0.1/tcp/0']
-        },
-        transports: [tcp()],
-        streamMuxers: [mplex()],
-        connectionEncryption: [noise()],
-        dht: kadDHT(),
-        peerDiscovery: [
-            bootstrap({
-                interval: 60e3,
-                list: bootstrapers
-            })
-        ],
-    })
+export const createNode = async (peerId = null) => {
+  if (peerId === null) {
+    peerId = await PeerId.create();
+  }
 
-    // start libp2p
-    await node.start()
-    console.log('libp2p has started')
+  const node = await createLibp2p({
+    peerId: peerId,
+    addresses: {
+      // add a listen address (localhost) to accept TCP connections on a random port
+      listen: ["/ip4/127.0.0.1/tcp/0"],
+    },
+    transports: [tcp()],
+    streamMuxers: [mplex()],
+    connectionEncryption: [noise()],
+    dht: kadDHT(),
+    peerDiscovery: [
+      bootstrap({
+        interval: 60e3,
+        list: bootstrapers,
+      }),
+    ],
+  });
 
-    // print out listening addresses
-    console.log('listening on addresses:')
-    node.getMultiaddrs().forEach((addr) => {
-        console.log(addr.toString())
-    })
+  // start libp2p
+  await node.start();
+  console.log("libp2p has started");
 
-    return node
-}
+  // print out listening addresses
+  console.log("listening on addresses:");
+  node.getMultiaddrs().forEach((addr) => {
+    console.log(addr.toString());
+  });
+
+  return node;
+};
 
 const stopNode = async (node) => {
-    // stop libp2p
-    await node.stop()
-    console.log('libp2p has stopped')
-}
+  // stop libp2p
+  await node.stop();
+  console.log("libp2p has stopped");
+};
 
-const login = async (node, username) => {
-    node.contentRouting.get(str2array(username))
+export const login = async (node, username) => {
+  node.contentRouting
+    .get(str2array(username))
     .then((data) => {
-        console.log("login data: ", array2str(data))
+      console.log("login data: ", array2str(data));
     })
     .catch((err) => {
-        console.log("login error: ", err)
-    })
-}
+      console.log("login error: ", err);
+    });
+};
 
-const register = async (node, username, password) => {
-    node.contentRouting.put(str2array(username), str2array(password))
-    .then((data) => {
-        console.log("register data: ", data)
+export const register = async (node, username, password) => {
+  usernameArray = str2array(username);
+
+  try {
+    await node.contentRouting.get(usernameArray);
+    return { error: "Username already used!" };
+  } catch (_) {
+    // do nothing
+  }
+
+  password = createHash('sha256').update(password).digest('hex');
+  content = {
+    password: password,
+  };
+
+  node.contentRouting
+    .put(usernameArray, str2array(JSON.stringify(content)))
+    .then((_) => {
+      return { data: "Successful register." };
     })
-    .catch((err) => {
-        console.log("register error: ", err)
-    })
-}
+    .catch((_) => {
+      // do nothing
+    });
+};
+
+const node = {
+  register,
+  login,
+};
+
+export default node;
