72a0809feb959fb40707c8305f88429ff15d2895
Beatriz Aguiar <beatrizaguiar.up@gmail.com>
2022-12-03 16:17:33 +0000

    implement frontend feed

--- frontend/src/Pages/Feed.jsx
+++ frontend/src/Pages/Feed.jsx
@@ -1,67 +1,150 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "../Components/Navbar";
 import api from "../Utils/api";
+import { IoMdSend } from "react-icons/io";
+import { AiFillDelete } from "react-icons/ai";
 
-export default function Feed({ logout }) {
-    const [port, setPort] = useState(3001);
-    const [feed, setFeed] = useState("");
-    const [post, setPost] = useState("");
+export default function Feed() {
+  const [port, setPort] = useState(3001);
+  const [feed, setFeed] = useState("");
+  const [post, setPost] = useState("");
 
-    const fetchFeed = () => {
-        api.get("feed/", port)
-            .then((res) => {
-                console.log("Response:", res.data);
-                // TODO: get complete feed
-                setFeed(res.data.feed[0].message);
-            })
-            .catch((err) => {
-                console.log("Error fetching feed:" + err);
-            });
-    };
+  const mockFeed = [
+    {
+      username: "Manu",
+      message: "Oi pipa",
+      date: Date.now(),
+    },
+    {
+      username: "Jones",
+      message: "Portugal ganhou",
+      date: new Date(2022, 11, 1, 3, 24, 0),
+    },
+    {
+      username: "Maria",
+      message: "Beleza",
+      date: new Date(2022, 11, 2, 3, 24, 0),
+    },
+    {
+      username: "maggy",
+      message: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec qu",
+      date: Date.now(),
+    }
+  ];
 
-    const onSubmitPost = (e) => {
-        e.preventDefault();
+  const fetchFeed = () => {
+    api
+      .get("feed/", port)
+      .then((res) => {
+        console.log("Response:", res.data);
+        // TODO: get complete feed
+        setFeed(res.data.feed[0].message);
+      })
+      .catch((err) => {
+        console.log("Error fetching feed:" + err);
+      });
+  };
 
-        api.post("post/", port, { message: post })
-            .then((res) => {
-                console.log("Post response", res.data);
-            })
-            .catch((err) => {
-                console.log("Post error:" + err);
-            });
-    };
+  const getTimeDiference = (currDate, date) => {
+    const secondDiff = (currDate - date) / 1000;
+    const minuteDiff = secondDiff / 60;
+    const hourDiff = minuteDiff / 60;
+    const dayDiff = hourDiff / 24;
+    const monthDiff = dayDiff / 30;
+    const yearDiff = monthDiff / 12;
 
-    const handleChange = (event) => {
-        const { value } = event.target;
-        setPost(value);
-    };
+    if (yearDiff >= 1) {
+      return Math.floor(yearDiff) + " years ago";
+    }
+    if (monthDiff >= 1) {
+      return Math.floor(monthDiff) + " months ago";
+    }
+    if (dayDiff >= 1) {
+      return Math.floor(dayDiff) + " days ago";
+    }
+    if (hourDiff >= 1) {    
+      return Math.floor(hourDiff) + " hours ago";
+    }
+    if (minuteDiff >= 1) {
+        return Math.floor(minuteDiff) + " minutes ago";
+    }
+    if (secondDiff >= 1) {
+        return Math.floor(secondDiff) + " seconds ago";
+    }
+    return "Just now";
+  };
+  
+  const handleDeletion = () => {
+    setPost("");
+  };
 
-    useEffect(() => {
-        const port = sessionStorage.getItem("port");
-        if (port) setPort(port);
+  const handlePost = (e) => { 
+    e.preventDefault();
 
-        fetchFeed();
-    }, []);
+    if (post.length > 200) return;
 
-    return (
-        <>
-            <NavBar logout={logout} />
-            <div className="container d-flex flex-column align-items-center h-100 mt-5">
-                <div className="row col-sm-8 col-md-6 col-12 ">
-                    <form onSubmit={onSubmitPost}>
-                        <div className="form-group mt-3">
-                            <textarea
-                                id="post"
-                                name="post"
-                                className="form-control rounded-pill bg-secondary border border-0"
-                                placeholder="What's piuing?"
-                                onChange={(e) => handleChange(e)}
-                            />
-                        </div>
-                    </form>
-                </div>
-                <div>{/* TODO: show complete feed */ feed}</div>
+    api
+    .post("post/", port, { message: post })
+    .then((res) => {
+      console.log("Post response", res.data);
+    })
+    .catch((err) => {
+      console.log("Post error:" + err);
+    });
+  };
+
+  useEffect(() => {
+    const port = sessionStorage.getItem("port");
+    if (port) setPort(port);
+
+    fetchFeed();
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <div className="container d-flex flex-column align-items-center mt-5">
+        <div className="row col-sm-7 col-lg-5 col-10">
+          <div>
+            <div className="form-group mt-3 position-relative">
+              <textarea
+                id="post"
+                name="post"
+                rows={4}
+                className="form-control rounded bg-secondary border-0 text-white px-3"
+                placeholder="What's piuing?"
+                value={post}
+                onChange={(e) => setPost(e.target.value)}
+              />
+              <div className="position-absolute d-flex end-0 bottom-0 justify-content-center align-items-center me-2 mb-1">
+                <span className="" style={{color: post.length > 200 ? "red" : "#15202B"}}>{post.length}</span>
+                <span className="me-2" style={{color: "#15202B"}}>/200</span>
+                <AiFillDelete className="me-2" style={{color: "#15202B", cursor: "pointer"}} onClick={handleDeletion}/>
+                <IoMdSend style={{color: "#15202B", cursor: "pointer"}} onClick={handlePost}/>
+              </div>
             </div>
-        </>
-    );
+          </div>
+        </div>
+        <div className="row col-sm-7 col-lg-5 col-10 justify-content-center my-5">
+            <hr style={{height: "3px", backgroundColor: "white"}} className="border border-0" />
+            {mockFeed.map((post) => {
+                const date = Date.now();
+                return (
+                <div className="row" key={"id "+ post.username + post.date.toString()}>
+                    <div className="card bg-dark text-white mt-3">
+                    <div className="card-body">
+                        <div className="d-flex flex-direction-row align-items-center">
+                            <h5 className="card-title me-2" style={{ fontWeight: 700 }}>{post.username}</h5>
+                            <small className="card-subtitle text-muted">{getTimeDiference(date, post.date)}</small>
+                        </div>
+                        <p className="card-text mt-2">{post.message}</p>
+                    </div>
+                    </div>
+                </div>
+                );
+            })}
+        </div>
+      </div>
+    </>
+  );
 }
