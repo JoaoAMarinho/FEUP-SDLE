f21ed4d09b610880d67737a153ae2deda714b4bb
JoaoMarinho <61231873+JoaoAMarinho@users.noreply.github.com>
2022-12-03 14:08:27 +0000

    Update login page

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import api from "../Utils/api";
 import Logo from "../logo.svg";
 
@@ -12,6 +13,7 @@ export default function Login() {
         username: "",
         password: "",
     });
+    const [showPassword, setShowPassword] = useState(false);
 
     const [validation, setValidation] = useState({
         username: "",
@@ -78,18 +80,18 @@ export default function Login() {
 
     return (
         <div className="container d-flex justify-content-center align-items-center flex-column h-100">
-            <div className="position-absolute top-0 start-0 mt-2 ms-5">
+            <div className="top-0 start-0 mt-2 ms-5 mb-3 mb-sm-0">
                 <img
                     src={Logo}
                     alt="PiuPiu Logo"
                     style={{ width: 130, height: 130 }}
                     className="img-fluid"
                 ></img>
-                <p className="mt-0" style={{ color: "#1D9BF0" }}>
+                <p className="mt-0 d-none d-sm-block" style={{ color: "#1D9BF0" }}>
                     Welcome to PiuPiu
                 </p>
             </div>
-            <div className="row col-sm-8 col-md-5 col-12 px-4">
+            <div className="row col-md-8 col-lg-6 col-xl-5 col-10 px-4">
                 <div className="col-12 mb-4">
                     <h1 className="w-100">Login</h1>
                 </div>
@@ -102,7 +104,7 @@ export default function Login() {
                             id="username"
                             name="username"
                             className={
-                                "form-control rounded-pill bg-secondary border border-0" +
+                                "form-control rounded-pill bg-secondary border border-0 text-white" +
                                 (validation.username ? " is-invalid" : "")
                             }
                             onChange={(e) => handleChange(e)}
@@ -118,31 +120,38 @@ export default function Login() {
                         <label htmlFor="password" className="mb-1">
                             Password
                         </label>
-                        <input
-                            id="password"
-                            type="password"
-                            name="password"
-                            className={
-                                "form-control rounded-pill bg-secondary border border-0" +
-                                (validation.password ? " is-invalid" : "")
-                            }
-                            onChange={(e) => handleChange(e)}
-                            value={inputValues.password}
-                        />
+                        <div className="d-flex flex-direction-row align-items-center position-relative">
+                            <input
+                                id="password"
+                                type={showPassword ? "text" : "password"}
+                                name="password"
+                                className={
+                                    "form-control rounded-pill bg-secondary border border-0 text-white" +
+                                    (validation.password ? " is-invalid" : "")
+                                }
+                                onChange={(e) => handleChange(e)}
+                                value={inputValues.password}
+                            />
+                            <div className="position-absolute end-0 me-2" onClick={()=>{setShowPassword(!showPassword);}} style={{zIndex: 1}}>
+                                { showPassword ? <FaEyeSlash id="hide_eye" /> : <FaEye id="show_eye" /> }
+                            </div>
+                        </div>
+                        
                         {validation.password && (
                             <div className="invalid-feedback">
                                 {"Error: " + validation.password}
                             </div>
                         )}
                     </div>
-                    <div className="form-group mt-5 d-flex justify-content-between">
+                    <div className="form-group mt-5 d-flex justify-content-between align-items-center">
                         <span>
                             Do not have an account yet?{" "}
-                            <a href="/register">Register</a>
+                            <a href="/register" style={{ color: "#1D9BF0" }}>Register</a>
                         </span>
                         <button
                             type="submit"
-                            className="btn btn-primary rounded-pill"
+                            className="btn btn-primary rounded-pill px-sm-5 px-3 py-2 ms-2 ms-sm-0"
+                            style={{ backgroundColor: "#1D9BF0", fontWeight: 500, fontSize: "1.1rem" }}
                         >
                             Login
                         </button>
